f3e623b50ac1faad2e660d1acd097f61275fc8f1
Tara <32590508+talyke@users.noreply.github.com>
2021-12-13 17:14:16 -0700

    wpm

--- srcs/stars.js
+++ srcs/stars.js
@@ -1,10 +1,15 @@
-import { incrementCustomProperty } from './updateCustomProperty.js'
+import { incrementCustomProperty, setCustomProperty } from './updateCustomProperty.js'
 
-const SPEED = .05
+const SPEED = 0.05
 const starsELems = document.querySelectorAll('[data-stars]')
+
+export function setupStars() {
+   setCustomProperty(starsElems[0], "--left", 0)
+   setCustomProperty(starsElems[1], "--left", 330)
+}
 
 export function updateStars(delta) {
    starsELems.forEach(stars => {
-  incrementCustomProperty(stars, "--left", delta * SPEED * -1)
+      incrementCustomProperty(stars, "--left", delta * SPEED * -1)
    })
 }
